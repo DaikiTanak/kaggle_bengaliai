dc76bb102ed0aea38977a6531f3295002f1a2807
DaikiTanak <daiki.yosky@gmail.com>
2020-02-19 23:02:04 +0900

    up to date

--- codes/queue.sh
+++ codes/queue.sh
@@ -8,8 +8,8 @@
 # python train_multi.py --name multi_resnet34_base --epoch 60 --batchsize 64
 # python train_multi.py --name multi_resnet34_mixup_alpha.3 --epoch 100 --mixup --mixup_alpha 0.3 --batchsize 64
 # python train.py --name resnet34_cutmix_alpha.1 --epoch 100 --cutmix --cutmix_alpha 0.1 --batchsize 256
-python train.py --model resnext101 --name resnext101_base --epoch 100 --batchsize 150
-python train.py --model resnext101 --name resnext101_cutmix_alpha.3 --epoch 150 --cutmix --cutmix_alpha 0.3 --batchsize 150
-python train.py --model resnext101 --name resnext101_cutmix_alpha.1 --epoch 150 --cutmix --cutmix_alpha 0.1 --batchsize 150
-python train.py --model resnext101 --name resnext101_mixup_alpha.1 --epoch 150 --mixup --mixup_alpha 0.1 --batchsize 150
-python train.py --model resnext101 --name resnext101_mixup_alpha.3 --epoch 150 --mixup --mixup_alpha 0.3 --batchsize 150
+python train.py --model resnext101 --name resnext101_base --epoch 100 --batchsize 100
+python train.py --model resnext101 --name resnext101_cutmix_alpha.3 --epoch 150 --cutmix --cutmix_alpha 0.3 --batchsize 100
+python train.py --model resnext101 --name resnext101_cutmix_alpha.1 --epoch 150 --cutmix --cutmix_alpha 0.1 --batchsize 100
+python train.py --model resnext101 --name resnext101_mixup_alpha.1 --epoch 150 --mixup --mixup_alpha 0.1 --batchsize 100
+python train.py --model resnext101 --name resnext101_mixup_alpha.3 --epoch 150 --mixup --mixup_alpha 0.3 --batchsize 100
