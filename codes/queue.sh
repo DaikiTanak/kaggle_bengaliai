8fb03e5a38727126357e367935f15de90e6c0f05
DaikiTanak <daiki.yosky@gmail.com>
2020-03-01 19:46:31 +0900

    fix queue

--- codes/queue.sh
+++ codes/queue.sh
@@ -10,15 +10,20 @@
 #
 # python train.py --model resnet34 --name resnet34_stratified_shift.01 --batchsize 500 --lr 0.1 --epoch 100 --affine_translate 0.01
 # python train.py --model resnet34 --name resnet34_stratified_shift.1 --batchsize 500 --lr 0.1 --epoch 100 --affine_translate 0.1
-python train.py --cutout --cutout_random --cutout_size 0.6 --model resnet34 --name resnet34_stratified_cutout.6_random --batchsize 500 --lr 0.1 --epoch 100
 
-python train.py --model resnet34 --name resnet34_stratified_shift.2 --batchsize 500 --lr 0.1 --epoch 100 --affine_translate 0.2
-python train.py --model resnet34 --name resnet34_stratified_shift.3 --batchsize 500 --lr 0.1 --epoch 100 --affine_translate 0.3
+# python train.py --cutout --cutout_random --cutout_size 0.6 --model resnet34 --name resnet34_stratified_cutout.6_random --batchsize 500 --lr 0.1 --epoch 100
+#
+# python train.py --model resnet34 --name resnet34_stratified_shift.2 --batchsize 500 --lr 0.1 --epoch 100 --affine_translate 0.2
 
-python train.py --model resnet34 --affine_rotate 8 --name resnet34_stratified_rotate8 --batchsize 500 --lr 0.1 --epoch 100 --affine_translate 0.1
+python train.py --model resnet34 --name resnet34_stratified_shift.4 --batchsize 500 --lr 0.1 --epoch 100 --affine_translate 0.4
+python train.py --model resnet34 --name resnet34_stratified_shift.5 --batchsize 500 --lr 0.1 --epoch 100 --affine_translate 0.5
+
 python train.py --model resnet34 --affine_rotate 5 --name resnet34_stratified_rotate5 --batchsize 500 --lr 0.1 --epoch 100 --affine_translate 0.1
+python train.py --model resnet34 --affine_rotate 8 --name resnet34_stratified_rotate8 --batchsize 500 --lr 0.1 --epoch 100 --affine_translate 0.1
 python train.py --model resnet34 --affine_rotate 11 --name resnet34_stratified_rotate11 --batchsize 500 --lr 0.1 --epoch 100 --affine_translate 0.1
+python train.py --model resnet34 --affine_rotate 15 --name resnet34_stratified_rotate15 --batchsize 500 --lr 0.1 --epoch 100 --affine_translate 0.1
 
+python train.py --model resnet34 --name resnet34_stratified_shift.3_cutout.6_random --batchsize 500 --lr 0.1 --epoch 150 --affine_translate 0.3 --cutout --cutout_random --cutout_size 0.6 --full_cv
 
 #python train.py --name resnet152_augment --model resnet152 --epoch 60 --lr 0.1
 #python train.py --name densenet_augment --model densenet --epoch 60 --lr 0.1
