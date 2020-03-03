abf5490eff0c262c12b7ac18c1175da9e0960f7c
DaikiTanak <daiki.yosky@gmail.com>
2020-03-03 18:42:02 +0900

    fix queue

--- codes/queue.sh
+++ codes/queue.sh
@@ -26,9 +26,8 @@
 
 
 python train.py --model resnet34 --name resnet34_stratified_shift.5_cutout.6_random --batchsize 500 --lr 0.1 --epoch 150 --affine_translate 0.5 --cutout --cutout_random --cutout_size 0.6 --affine_rotate 11
-python train.py --model densenet --name densenet_stratified_shift.5 --batchsize 200 --lr 0.1 --epoch 150 --affine_translate 0.5 --affine_rotate 11
-
-
+# python train.py --model densenet --name densenet_stratified_shift.5 --batchsize 200 --lr 0.1 --epoch 150 --affine_translate 0.5 --affine_rotate 11
+python train.py --model resnet34 --name resnet34_stratified_random_erasing_sl.25_sh_.4 --random_erasing --sl 0.25 --sh 0.4 --batchsize 500 --lr 0.1 --epoch 150
 
 
 
